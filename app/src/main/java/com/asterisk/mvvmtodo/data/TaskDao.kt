9c962bf70d6d573ca64629bf6d379f63133865e5
kelechi <oparaiskelechi@gmail.com>
2021-08-21 13:58:24 +0100

    implement ability to combine Room query

--- app/src/main/java/com/asterisk/mvvmtodo/data/TaskDao.kt
+++ app/src/main/java/com/asterisk/mvvmtodo/data/TaskDao.kt
@@ -1,13 +1,23 @@
 package com.asterisk.mvvmtodo.data
 
 import androidx.room.*
+import com.asterisk.mvvmtodo.ui.task.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
 
-    @Query("SELECT * FROM task_table WHERE name LIKE '%' || :searchQuery || '%' ORDER BY important DESC")
-    fun getTasks(searchQuery: String): Flow<List<Task>>
+    fun getTasks(query: String, sortOrder: SortOrder, hideCompleted: Boolean): Flow<List<Task>> =
+        when(sortOrder) {
+            SortOrder.BY_NAME -> getTasksSortedByName(query, hideCompleted)
+            SortOrder.BY_DATE -> getTasksSortedByDate(query, hideCompleted)
+        }
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, name")
+    fun getTasksSortedByName(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, created")
+    fun getTasksSortedByDate(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTask(task: Task)
--- app/src/main/java/com/asterisk/mvvmtodo/ui/task/TaskViewModel.kt
+++ app/src/main/java/com/asterisk/mvvmtodo/ui/task/TaskViewModel.kt
@@ -4,20 +4,34 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.asterisk.mvvmtodo.data.TaskDao
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
-import javax.inject.Inject
 
 
 class TaskViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ): ViewModel() {
 
+    @ExperimentalCoroutinesApi
     val searchQuery = MutableStateFlow("")
 
-    private val taskFlow = searchQuery.flatMapLatest {
-        taskDao.getTasks(it)
+    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+
+    val hideCompleted = MutableStateFlow(false)
+
+    @ExperimentalCoroutinesApi
+    private val taskFlow = combine(
+        searchQuery, sortOrder, hideCompleted
+    ) {query, sortOrder, hideCompleted ->
+        Triple(query, sortOrder, hideCompleted)
+    }.flatMapLatest { (query, sortOrder, hideComplete) ->
+        taskDao.getTasks(query, sortOrder, hideComplete)
     }
 
+    @ExperimentalCoroutinesApi
     val task = taskFlow .asLiveData()
 }
+
+enum class SortOrder {BY_NAME, BY_DATE}
